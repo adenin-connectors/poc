0dd80b7e9f97bee3be8b3b225399b3ba3ab31a30
snt_danilo_markovic <dmarkovic@smartnet.rs>
2019-03-21 10:40:13 +0100

    refactored code.

--- activities/orderVolumeByCountry.js
+++ activities/orderVolumeByCountry.js
@@ -3,6 +3,16 @@ const api = require('./common/api');
 
 module.exports = async (activity) => {
   let currentYear = new Date().getFullYear();
+
+  let maxVal = 99 * 100;
+  let randomData0 = [];
+  let randomData1 = [];
+
+  for (let i = 0; i < 3; i++) {
+    randomData0.push(getRandomInt(maxVal));
+    randomData1.push(getRandomInt(maxVal));
+  }
+
   try {
     activity.Response.Data = {
       chart: {
@@ -12,11 +22,11 @@ module.exports = async (activity) => {
             datasets: [
               {
                 label: currentYear - 1,
-                data: [getRandomInt(100) * 100, getRandomInt(100) * 100, getRandomInt(100) * 100]
+                data: randomData0
               },
               {
                 label: currentYear,
-                data: [getRandomInt(100) * 100, getRandomInt(100) * 100, getRandomInt(100) * 100]
+                data: randomData1
               }
             ]
           },
