73e865226acd0d3a19747b118597ea8c9cb66759
Martin Amm <marint.amm@adenin.com>
2019-03-24 14:41:36 -0400

    ping with T() prototype

---
+++ activities/ping.js
@@ -0,0 +1,17 @@
+'use strict';
+
+const api = require('./common/api');
+
+module.exports = async function (activity) {
+  
+  try {    
+
+    activity.Response.Data = {
+      success: true,
+      welcome: T('My name is {0} and I am from {1}.', 'Sam Adams', 'Boston')
+    };
+  } catch (error) {
+    Activity.handleError(error);
+    activity.Response.Data.success = false;
+  }
+};
