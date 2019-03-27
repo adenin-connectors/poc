853dd1aa91e755ed7a81b84bdd5c092de88573a5
olliejm <olliejm@protonmail.com>
2019-03-27 16:41:51 +0000

    add azure-pipelines.yml

--- activities/common/generator.js
+++ activities/common/generator.js
@@ -1,24 +1,20 @@
-var faker = require('faker');
+'use strict';
+
+const faker = require('faker');
 
 module.exports = {
-
   randomEntry: function (arr) {
-
     const rnd = Math.floor(0.5 + Math.random() * 100);
     const i = rnd % arr.length;
 
     return arr[i];
   },
-
-  detailUrl: function (enityName) {
-    return "https://www.adenin.com/pocdef";
+  detailUrl: function () {
+    return 'https://www.adenin.com/pocdef';
   },
-
   teamMember: function () {
-    var tm = {
+    return {
       name: faker.name.findName()
-    }
-    return tm;
+    };
   }
-
-}
+};
