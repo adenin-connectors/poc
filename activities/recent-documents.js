a311b017425c5a13a7001caab59801756fd48252
snt_danilo_markovic <dmarkovic@smartnet.rs>
2019-04-25 16:17:08 +0200

    Fixed time formating to expected time format.

--- activities/recent-documents.js
+++ activities/recent-documents.js
@@ -12,35 +12,35 @@ module.exports = async (activity) => {
         title: `Budget Plan ${d.getFullYear() + 1}`,
         description: `description 0`,
         link: generator.detailUrl(),
-        date: new Date(d.getFullYear(), d.getMonth(), d.getDate(), 8, 0, 0).toUTCString()
+        date: new Date(d.getFullYear(), d.getMonth(), d.getDate(), 8, 0, 0).toISOString()
       },
       {
         id: "1",
         title: `Meeting Notes Project 35345`,
         description: `description 1`,
         link: generator.detailUrl(),
-        date: new Date(d.getFullYear(), d.getMonth(), d.getDate() - 1, 15, 0, 0).toUTCString()
+        date: new Date(d.getFullYear(), d.getMonth(), d.getDate() - 1, 15, 0, 0).toISOString()
       },
       {
         id: "2",
         title: `Sales Report ${d.getMonth() - 1}.${d.getFullYear()}`,
         description: `description 2`,
         link: generator.detailUrl(),
-        date: new Date(d.getFullYear(), d.getMonth(), 1, 10, 0, 0).toUTCString()
+        date: new Date(d.getFullYear(), d.getMonth(), 1, 10, 0, 0).toISOString()
       },
       {
         id: "3",
         title: `Form 232 Draft`,
         description: `description 3`,
         link: generator.detailUrl(),
-        date: new Date(d.getFullYear(), d.getMonth(), -1, 11, 0, 0).toUTCString()
+        date: new Date(d.getFullYear(), d.getMonth(), -1, 11, 0, 0).toISOString()
       },
       {
         id: "4",
         title: `Sales Report ${d.getMonth() - 2}.${d.getFullYear()}`,
         description: `description 4`,
         link: generator.detailUrl(),
-        date: new Date(d.getFullYear(), d.getMonth() - 1, 1, 14, 0, 0).toUTCString()
+        date: new Date(d.getFullYear(), d.getMonth() - 1, 1, 14, 0, 0).toISOString()
       }
     ];
 
