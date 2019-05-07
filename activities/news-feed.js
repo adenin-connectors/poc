8c17d2d6dd1a266f51f16c6fe1b044bd925d8a6a
snt_danilo_markovic <dmarkovic@smartnet.rs>
2019-05-07 15:34:14 +0200

    Added pagination and daterange fitlering.

--- activities/news-feed.js
+++ activities/news-feed.js
@@ -8,36 +8,39 @@ module.exports = async (activity) => {
       {
         id: "1054889",
         title: `Grand Opening of a New Plant in China`,
-        description: `description 0`,
         link: generator.detailUrl()
       },
       {
         id: "1054891",
         title: `Opening of new logistics and distribution center`,
-        description: `description 1`,
         link: generator.detailUrl()
       },
       {
         id: "1054878",
         title: `New Wi-Fi enabled product line`,
-        description: `description 2`,
         link: generator.detailUrl()
       },
       {
         id: "1054880",
         title: `Leading with passion, mistakes and simplicity`,
-        description: `description 3`,
         link: generator.detailUrl()
       },
       {
         id: "1054893",
         title: `Multi-Year Agreement signed`,
-        description: `description 4`,
         link: generator.detailUrl()
       }
     ];
 
-    activity.Response.Data.items = getItemsBasedOnDayOfTheYear(activity, items);
+    let sortedItems = getItemsBasedOnDayOfTheYear(activity, items);
+
+    var dateRange = $.dateRange(activity, "today");
+    let filteredItems = filterItemsByDateRange(sortedItems, dateRange);
+
+    const pagination = $.pagination(activity);
+    let paginatedItems = paginateItems(filteredItems, pagination);
+
+    activity.Response.Data.items = paginatedItems;
     let value = activity.Response.Data.items.length;
     activity.Response.Data.title = T(activity, 'News Feed');
     activity.Response.Data.link = generator.detailUrl();
@@ -50,63 +53,91 @@ module.exports = async (activity) => {
     $.handleError(activity, error);
   }
 };
-
 function getRandomMinutes() {
   return (Math.floor(Math.random() * 60) + 1);
 }
-
 //** returns new item[] reordered based on day of the year */
 function getItemsBasedOnDayOfTheYear(activity, items) {
   let d = new Date();
-  let _8AMtime = new Date(d.getFullYear(), d.getMonth(), d.getDate(), 8, 0, 0);
-  let _2PMtime = new Date(d.getFullYear(), d.getMonth(), d.getDate(), 14, 0, 0);
-
-  let userLocalTime = moment(d)
-    .tz(activity.Context.UserTimezone);
+  let timeslot1 = new Date(d.getFullYear(), d.getMonth(), d.getDate(), 8, 0, 0);
+  let timeslot2 = new Date(d.getFullYear(), d.getMonth(), d.getDate(), 14, 0, 0);
+  let userLocalTime = moment(d).tz(activity.Context.UserTimezone);
 
   let daysOffset = 0; //number of days to offset current date (now - daysOffset)
-  let is2PMtime = null; // keeps track of which time to assign to news next (8am or 2pm)
+  let isTimeslot2 = null; // keeps track of which time to assign to news next (timeslot1 or timeslot2)
+
   if (userLocalTime.hours() >= 16) {
-    is2PMtime = true;
+    isTimeslot2 = true;
   } else if (userLocalTime.hours() >= 10) {
-    is2PMtime = false;
+    isTimeslot2 = false;
   } else {
     daysOffset = -1;
-    is2PMtime = true;
+    isTimeslot2 = true;
   }
 
   let zeroBasedDayInYear = ((Date.UTC(d.getFullYear(), d.getMonth(), d.getDate()) - Date.UTC(d.getFullYear(), 0, 0)) / 24 / 60 / 60 / 1000) - 1;
   let startIndex = zeroBasedDayInYear % items.length;
 
   let sortedItems = [];
-  let daysCounter = 0; // used to count number of news in a day and help generate (date -1),...,(date -n).
+  let newsCounter = 0; // used to count number of news in a day and help generate (date -1),...,(date -n).
   for (let i = 0; i < items.length; i++) {
     if (startIndex >= items.length) {
       startIndex = 0;
     }
+
     let timeToAssign = null;
-    daysCounter++;
-    if (is2PMtime) {
-      timeToAssign = new Date(_2PMtime);
-      if (daysCounter >= 2) { // keeps track of number of news and increases days offset when needed
-        daysCounter = 0;
+    newsCounter++;
+    if (isTimeslot2) {
+      timeToAssign = new Date(timeslot2);
+      if (newsCounter >= 2) { // keeps track of number of news and increases days offset when needed
+        newsCounter = 0;
         daysOffset--;
       }
     } else {
-      timeToAssign = new Date(_8AMtime);
+      timeToAssign = new Date(timeslot1);
     }
 
     timeToAssign.setDate(timeToAssign.getDate() + daysOffset);
     timeToAssign.setMinutes(getRandomMinutes());
-    is2PMtime = !is2PMtime; // switch next time to assign
+    isTimeslot2 = !isTimeslot2; // switch time to assign
 
-    let newsDate = moment(timeToAssign)
-      .tz(activity.Context.UserTimezone);
+    let newsDate = moment(timeToAssign).tz(activity.Context.UserTimezone);
 
-    items[startIndex].date = newsDate.toISOString();
+    items[startIndex].date = newsDate.toLocaleString();
     sortedItems.push(items[startIndex]);
     startIndex++;
-
   }
+
   return sortedItems;
+}
+//** filters provided items[] based on provided daterange */
+function filterItemsByDateRange(items, daterange) {
+  let filteredItems = [];
+  let start = new Date(daterange.startDate).valueOf();
+  let end = new Date(daterange.endDate).valueOf();
+
+  for (let i = 0; i < items.length; i++) {
+    let tmpDate = new Date(items[i].date).valueOf();
+    if (start < tmpDate && tmpDate < end) {
+      filteredItems.push(items[i]);
+    }
+  }
+
+  return filteredItems;
+}
+//** paginate items[] based on provided pagination */
+function paginateItems(items, pagination) {
+  let pagiantedItems = [];
+  const pageSize = parseInt(pagination.pageSize);
+  const offset = (parseInt(pagination.page) - 1) * pageSize;
+
+  if (offset > items.length) return pagiantedItems;
+
+  for (let i = offset; i < offset + pageSize; i++) {
+    if (i >= items.length) {
+      break;
+    }
+    pagiantedItems.push(items[i]);
+  }
+  return pagiantedItems;
 }
