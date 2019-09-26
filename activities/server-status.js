40e327548b3fb4b5791772a9f1d0a8becafd23ed
olliejm <olliejm@protonmail.com>
2019-09-26 10:16:16 +0100

    fix server status briefing message logic and text

--- activities/server-status.js
+++ activities/server-status.js
@@ -6,42 +6,41 @@ const shared = require('./common/shared');
 
 module.exports = async function (activity) {
   try {
-
-    var down = [ faker.random.boolean(), faker.random.boolean(), faker.random.boolean() ]
-
+    const down = [faker.random.boolean(), faker.random.boolean(), faker.random.boolean()];
     const servers = [
       {
         _type: 'server-status',
-        id: 'Mars',        
-        title: 'Mars: '+ (down[0] ? 'down' : 'up'),   
-        color: down[0] ? 'red' : 'green',     
+        id: 'Mars',
+        title: 'Mars: ' + (down[0] ? 'down' : 'up'),
+        color: down[0] ? 'red' : 'green',
         date: new Date().toISOString(),
         link: generator.detailUrl()
       },
       {
         _type: 'server-status',
         id: 'Jupiter',
-        title: 'Jupiter: ' +  (down[1] ? 'down' : 'up'),   
-        color: down[1] ? 'red' : 'green',     
+        title: 'Jupiter: ' + (down[1] ? 'down' : 'up'),
+        color: down[1] ? 'red' : 'green',
         date: new Date().toISOString(),
         link: generator.detailUrl()
       },
       {
         _type: 'server-status',
         id: 'Saturn',
-        title: 'Saturn: ' +  (down[2] ? 'down' : 'up'), 
-        color: down[2] ? 'red' : 'green',     
+        title: 'Saturn: ' + (down[2] ? 'down' : 'up'),
+        color: down[2] ? 'red' : 'green',
         date: new Date().toISOString(),
         link: generator.detailUrl()
       }
     ];
 
-    let downCount = 0;
+    const serversDown = [];
 
     for (let i = 0; i < servers.length; i++) {
-      if (down[i]) downCount++;
+      if (down[i]) serversDown.push(servers[i]);
     }
 
+    const downCount = serversDown.length;
     const response = activity.Response.Data;
 
     response.items = servers;
@@ -53,22 +52,23 @@ module.exports = async function (activity) {
     response.thumbnail = 'https://www.adenin.com/assets/images/wp-images/logo/freshping.svg';
 
     if (downCount > 0) {
-
-      response.description = downCount > 1 ? T(activity, '{0} servers are currently down.', downCount) : T(activity, '1 server is currently down.');
+      response.description = downCount > 1 ?
+        T(activity, '{0} servers are currently down.', downCount) :
+        T(activity, '1 server is currently down.');
 
       response.value = downCount;
       response.date = shared.getHighestDate(servers);
       response.color = 'red';
 
       switch (downCount) {
-        case 1:
-          response.briefing = T(activity, `Server <b>${response.items[0].id}</b> is currently down.`);
-          break;
-        case 2:
-          response.briefing = T(activity, `Server <b>${response.items[0].id}</b> and <b>${downCount - 1}</b> more are currently down.`);
-          break;
-        default:
-          response.briefing = T(activity, `Server <b>${response.items[0].id}</b>, <b>${response.items[1].title}</b> and <b>${downCount - 2}</b> more are currently down.`);
+      case 1:
+        response.briefing = T(activity, `Down: <b>${serversDown[0].id}</b>.`);
+        break;
+      case 2:
+        response.briefing = T(activity, `Down: <b>${serversDown[0].id}</b> and <b>${serversDown[1].id}</b>.`);
+        break;
+      default:
+        response.briefing = T(activity, `Down: <b>${serversDown[0].id}</b> and <b>${downCount - 1}</b> more.`);
       }
     } else {
       response.description = T(activity, 'All servers are running.');
