edb3386b8895d4551b8d924f8a4d1bc9173604c1
Martin Amm <martin.amm@adenin.com>
2019-11-11 03:38:04 -0500

    prototype readDate processing, returning payload _hash

--- activities/myleads.js
+++ activities/myleads.js
@@ -2,17 +2,20 @@
 const faker = require('faker');
 const generator = require('./common/generator');
 const shared = require('./common/shared');
+const md5 = require('md5');
 
 module.exports = async function (activity) {
   try {
     const pagination = $.pagination(activity);
-    let numberToGenerate = parseInt(pagination.pageSize)*2;
-
+    let numberToGenerate = parseInt(pagination.pageSize) * 2;
+    let count = 0;
     let items = [];
+    let readDate = (new Date(new Date().setDate(new Date().getDate() - 30))).toISOString(); // default read date 30 days in the past
+    if (activity.Request.Query.readDate) readDate =activity.Request.Query.readDate;
 
     for (let i = 0; i < numberToGenerate; i++) {
       let d = new Date();
-      d.setMinutes(d.getMinutes() - (i+1)*25);
+      d.setMinutes(d.getMinutes() - (i + 1) * (process.env.NODE_ENV=="development" ? 3 : 25)); // shorter interval to debug readDate
       const item = {
         id: i.toString(),
         title: faker.name.findName(),
@@ -20,29 +23,34 @@ module.exports = async function (activity) {
         link: generator.detailUrl(),
         date: d.toISOString()
       };
+
+      if (item.date > readDate) count++;
+
       items.push(item);
     }
 
     var dateRange = $.dateRange(activity);
     items = shared.filterItemsByDateRange(items, dateRange);
-    let value = items.length;
+
 
     items = shared.paginateItems(items, pagination);
 
+    // return explicit _hash 
+    activity.Response.Data._hash = md5(JSON.stringify(items));
+    
     activity.Response.Data.items = items;
     activity.Response.Data.title = T(activity, 'Leads');
     activity.Response.Data.link = generator.detailUrl();
     activity.Response.Data.linkLabel = T(activity, 'All Leads');
 
-    activity.Response.Data.actionable = value > 0;
+    activity.Response.Data.actionable = items.length > 0;
 
     activity.Response.Data.thumbnail = "https://www.adenin.com/assets/images/wp-images/logo/salesforce.svg";
 
-    if (value > 0) {
-      activity.Response.Data.value = value;
+    if (count > 0) {
+      activity.Response.Data.value = count;
       activity.Response.Data.date = shared.getHighestDate(items);
-      activity.Response.Data.description = value > 1 ? T(activity, "You have {0} leads.", value) :
-        T(activity, "You have 1 lead.");
+      activity.Response.Data.description = count > 1 ? T(activity, "You have {0} leads.", count) : T(activity, "You have 1 lead.");
       activity.Response.Data.briefing = activity.Response.Data.description + " The latest is <b>" + activity.Response.Data.items[0].title + "</b>.";
     } else {
       activity.Response.Data.description = T(activity, `You have no leads.`);
