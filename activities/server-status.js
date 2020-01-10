042003d6d80f985a42f6f5c887ac099312d59901
olliejm <olliejm@protonmail.com>
2020-01-10 13:23:10 +0000

    increase in d value seemed to worsen problem

--- activities/server-status.js
+++ activities/server-status.js
@@ -25,8 +25,8 @@ module.exports = async function (activity) {
 
     // N = 3 servers
     const N = servers.length;
-    // d = 12 minutes
-    const d = 720000;
+    // d = 4 minutes
+    const d = 240000;
     // interval for t0 change (2^N)*d
     const interval = (Math.pow(2, N)) * d;
     // current ms since epoch
