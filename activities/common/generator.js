5b60931c791b2e55a066a35df47372de7d07c270
olliejm <olliejm@protonmail.com>
2020-01-20 11:53:10 +0000

    rotate leads based on time of day and day in year

--- activities/common/generator.js
+++ activities/common/generator.js
@@ -2,6 +2,8 @@
 
 const faker = require('faker');
 
+const detailUrl = 'https://www.adenin.com/pocdef';
+
 module.exports = {
   randomEntry: function (arr) {
     const rnd = Math.floor(0.5 + Math.random() * 100);
@@ -9,12 +11,140 @@ module.exports = {
 
     return arr[i];
   },
-  detailUrl: function () {
-    return 'https://www.adenin.com/pocdef';
-  },
+  detailUrl: () => detailUrl,
   teamMember: function () {
     return {
       name: faker.name.findName()
     };
-  }
+  },
+  leadsList: () => [
+    {
+      id: '32101',
+      title: 'Mr. Marlon Schuppe',
+      description: 'Maggio - Marks',
+      thumbnail: 'https://s3.amazonaws.com/uifaces/faces/twitter/ssiskind/128.jpg',
+      imageIsAvatar: true,
+      link: detailUrl
+    },
+    {
+      id: '32102',
+      title: 'Cole Kirlin',
+      description: 'Grady Inc',
+      thumbnail: 'https://s3.amazonaws.com/uifaces/faces/twitter/herrhaase/128.jpg',
+      imageIsAvatar: true,
+      link: detailUrl
+    },
+    {
+      id: '32103',
+      title: 'Myrl Kovacek',
+      description: 'David - Kuhlman',
+      thumbnail: 'https://s3.amazonaws.com/uifaces/faces/twitter/vytautas_a/128.jpg',
+      imageIsAvatar: true,
+      link: detailUrl
+    },
+    {
+      id: '32104',
+      title: 'Haven Greenfelder',
+      description: 'Williamson, Torp and Koepp',
+      thumbnail: 'https://s3.amazonaws.com/uifaces/faces/twitter/macxim/128.jpg',
+      imageIsAvatar: true,
+      link: detailUrl
+    },
+    {
+      id: '32105',
+      title: 'Raphaelle Jaskolski',
+      description: 'Hayes Inc',
+      thumbnail: 'https://s3.amazonaws.com/uifaces/faces/twitter/aislinnkelly/128.jpg',
+      imageIsAvatar: true,
+      link: detailUrl
+    },
+    {
+      id: '32106',
+      title: 'Rebecca Collins',
+      description: 'Stanton - Gusikowski',
+      thumbnail: 'https://s3.amazonaws.com/uifaces/faces/twitter/rpatey/128.jpg',
+      imageIsAvatar: true,
+      link: detailUrl
+    },
+    {
+      id: '32107',
+      title: 'Rigoberto Okuneva',
+      description: 'Leannon - Roob',
+      thumbnail: 'https://s3.amazonaws.com/uifaces/faces/twitter/emmeffess/128.jpg',
+      imageIsAvatar: true,
+      link: detailUrl
+    },
+    {
+      id: '32108',
+      title: 'Daniella Kub',
+      description: 'McLaughlin, Cummerata and Crona',
+      thumbnail: 'https://s3.amazonaws.com/uifaces/faces/twitter/maiklam/128.jpg',
+      imageIsAvatar: true,
+      link: detailUrl
+    },
+    {
+      id: '32109',
+      title: 'Sedrick O\'Connell',
+      description: 'Waters Group',
+      thumbnail: 'https://s3.amazonaws.com/uifaces/faces/twitter/doronmalki/128.jpg',
+      imageIsAvatar: true,
+      link: detailUrl
+    },
+    {
+      id: '32110',
+      title: 'Nelson Berge',
+      description: 'Kutch - Flatley',
+      thumbnail: 'https://s3.amazonaws.com/uifaces/faces/twitter/lowie/128.jpg',
+      imageIsAvatar: true,
+      link: detailUrl
+    },
+    {
+      id: '32111',
+      title: 'Cristina Medhurst',
+      description: 'Haag - Boehm',
+      thumbnail: 'https://s3.amazonaws.com/uifaces/faces/twitter/larrybolt/128.jpg',
+      imageIsAvatar: true,
+      link: detailUrl
+    },
+    {
+      id: '32112',
+      title: 'Hilma Strosin',
+      description: 'Gorczany, Koch and Schowalter',
+      thumbnail: 'https://s3.amazonaws.com/uifaces/faces/twitter/ddggccaa/128.jpg',
+      imageIsAvatar: true,
+      link: detailUrl
+    },
+    {
+      id: '32113',
+      title: 'Randy Kshlerin',
+      description: 'Von, Huel and MacGyver',
+      thumbnail: 'https://s3.amazonaws.com/uifaces/faces/twitter/artem_kostenko/128.jpg',
+      imageIsAvatar: true,
+      link: detailUrl
+    },
+    {
+      id: '32114',
+      title: 'Blaze Hoeger',
+      description: 'Schmitt - Ratke',
+      thumbnail: 'https://s3.amazonaws.com/uifaces/faces/twitter/nsamoylov/128.jpg',
+      imageIsAvatar: true,
+      link: detailUrl
+    },
+    {
+      id: '32115',
+      title: 'Abdul Carroll',
+      description: 'Schowalter LLC',
+      thumbnail: 'https://s3.amazonaws.com/uifaces/faces/twitter/jacobbennett/128.jpg',
+      imageIsAvatar: true,
+      link: detailUrl
+    },
+    {
+      id: '32116',
+      title: 'Eriberto White',
+      description: 'Littel and Sons',
+      thumbnail: 'https://s3.amazonaws.com/uifaces/faces/twitter/newbrushes/128.jpg',
+      imageIsAvatar: true,
+      link: detailUrl
+    }
+  ]
 };
--- activities/leads-new.js
+++ activities/leads-new.js
@@ -8,58 +8,8 @@ const shared = require('./common/shared');
 
 module.exports = async (activity) => {
   try {
-    const items = [
-      {
-        id: '32101',
-        title: 'Mr. Marlon Schuppe',
-        description: 'Maggio - Marks',
-        thumbnail: 'https://s3.amazonaws.com/uifaces/faces/twitter/ssiskind/128.jpg',
-        imageIsAvatar: true,
-        link: generator.detailUrl()
-      },
-      {
-        id: '32102',
-        title: 'Cole Kirlin',
-        description: 'Grady Inc',
-        thumbnail: 'https://s3.amazonaws.com/uifaces/faces/twitter/herrhaase/128.jpg',
-        imageIsAvatar: true,
-        link: generator.detailUrl()
-      },
-      {
-        id: '32103',
-        title: 'Myrl Kovacek',
-        description: 'David - Kuhlman',
-        thumbnail: 'https://s3.amazonaws.com/uifaces/faces/twitter/vytautas_a/128.jpg',
-        imageIsAvatar: true,
-        link: generator.detailUrl()
-      },
-      {
-        id: '32104',
-        title: 'Haven Greenfelder',
-        description: 'Williamson, Torp and Koepp',
-        thumbnail: 'https://s3.amazonaws.com/uifaces/faces/twitter/macxim/128.jpg',
-        imageIsAvatar: true,
-        link: generator.detailUrl()
-      },
-      {
-        id: '32105',
-        title: 'Raphaelle Jaskolski',
-        description: 'Hayes Inc',
-        thumbnail: 'https://s3.amazonaws.com/uifaces/faces/twitter/aislinnkelly/128.jpg',
-        imageIsAvatar: true,
-        link: generator.detailUrl()
-      },
-      {
-        id: '32106',
-        title: 'Rebecca Collins',
-        description: 'Stanton - Gusikowski',
-        thumbnail: 'https://s3.amazonaws.com/uifaces/faces/twitter/rpatey/128.jpg',
-        imageIsAvatar: true,
-        link: generator.detailUrl()
-      }
-    ];
-
-    const sortedItems = getItemsBasedOnDayOfTheYear(activity, items);
+    const items = generator.leadsList();
+    const sortedItems = sortItemsBasedOnTimeOfDay(activity, items);
 
     const dateRange = $.dateRange(activity);
     const filteredItems = shared.filterItemsByDateRange(sortedItems, dateRange);
@@ -85,7 +35,7 @@ module.exports = async (activity) => {
     activity.Response.Data.title = T(activity, 'New Leads');
     activity.Response.Data.link = generator.detailUrl();
     activity.Response.Data.linkLabel = T(activity, 'All Leads');
-    activity.Response.Data.actionable = items.length > 0;
+    activity.Response.Data.actionable = count > 0;
     activity.Response.Data.thumbnail = 'https://www.adenin.com/assets/images/wp-images/logo/salesforce.svg';
 
     if (count > 0) {
@@ -101,69 +51,91 @@ module.exports = async (activity) => {
   }
 };
 
-const morningHour = 9;
-const morningMinutes = 35;
-const afternoonHour = 13;
-const afternoonMinutes = 5;
-
-//** returns new item[] reordered based on day of the year */
-function getItemsBasedOnDayOfTheYear(activity, items) {
-  const timeslot1 = moment().tz(activity.Context.UserTimezone).hours(morningHour).minutes(morningMinutes);
-  const timeslot2 = moment().tz(activity.Context.UserTimezone).hours(afternoonHour).minutes(afternoonMinutes);
-
+function sortItemsBasedOnTimeOfDay(activity, items) {
   const userLocalTime = moment().tz(activity.Context.UserTimezone);
+
+  const slot1 = userLocalTime.clone().hours(9).minutes(30).startOf('minute');
+  const slot2 = userLocalTime.clone().hours(11).startOf('hour');
+  const slot3 = userLocalTime.clone().hours(12).minutes(30).startOf('minute');
+  const slot4 = userLocalTime.clone().hours(14).startOf('hour');
+  const slot5 = userLocalTime.clone().hours(15).minutes(30).startOf('minute');
+  const slot6 = userLocalTime.clone().hours(17).startOf('hour');
+
+  let indexShift = 0;
+  let daysOffset = 0;
+
+  if (userLocalTime.isBefore(slot1)) {
+    daysOffset = -1;
+  } else if (userLocalTime.isSameOrAfter(slot1) && userLocalTime.isSameOrBefore(slot2)) {
+    indexShift = 1;
+  } else if (userLocalTime.isBefore(slot3)) {
+    indexShift = 2;
+  } else if (userLocalTime.isBefore(slot4)) {
+    indexShift = 3;
+  } else if (userLocalTime.isBefore(slot5)) {
+    indexShift = 4;
+  } else if (userLocalTime.isBefore(slot6)) {
+    indexShift = 5;
+  } else {
+    indexShift = 6;
+  }
 
   const d = new Date();
 
-  let daysOffset = 0; //number of days to offset current date (now - daysOffset)
-  let isTimeslot2 = null; // keeps track of which time to assign to news next (timeslot1 or timeslot2)
-
-  if (userLocalTime.hours() >= (afternoonHour + 1)) {
-    isTimeslot2 = true;
-  } else if (userLocalTime.hours() >= (morningHour + 1)) {
-    isTimeslot2 = false;
-  } else {
-    daysOffset = -1;
-    isTimeslot2 = true;
-  }
-
   const zeroBasedDayInYear = ((Date.UTC(d.getFullYear(), d.getMonth(), d.getDate()) - Date.UTC(d.getFullYear(), 0, 0)) / 24 / 60 / 60 / 1000) - 1;
-  let startIndex = zeroBasedDayInYear % items.length;
-
-  if (isTimeslot2) startIndex++;
+  let startIndex = (zeroBasedDayInYear % items.length) + indexShift;
 
   const sortedItems = [];
-  let counter = 0; // used to count number of news in a day and help generate (date -1),...,(date -n).
 
   for (let i = 0; i < items.length; i++) {
     if (startIndex >= items.length) startIndex = 0;
     else if (startIndex < 0) startIndex = items.length - 1;
 
     let timeToAssign = null;
-    counter++;
 
-    if (isTimeslot2) {
-      timeToAssign = timeslot2.clone();
-
-      if (counter >= 2) { // keeps track of number of news and increases days offset when needed
-        counter = 0;
-        daysOffset--;
-      }
-    } else {
-      timeToAssign = timeslot1.clone();
+    switch (indexShift) {
+    case 1:
+      timeToAssign = slot1.clone();
+      break;
+    case 2:
+      timeToAssign = slot2.clone();
+      break;
+    case 3:
+      timeToAssign = slot3.clone();
+      break;
+    case 4:
+      timeToAssign = slot4.clone();
+      break;
+    case 5:
+      timeToAssign = slot5.clone();
+      break;
+    case 6:
+      timeToAssign = slot6.clone();
+      break;
+    default:
+      daysOffset--;
+      timeToAssign = slot6.clone();
+      break;
     }
 
-    timeToAssign.date(timeToAssign.date() + daysOffset);
-    timeToAssign.startOf('minute');
-
-    isTimeslot2 = !isTimeslot2; // switch time to assign
+    timeToAssign.date(timeToAssign.date() + daysOffset).startOf('minute');
 
     const date = timeToAssign.clone().utc();
 
     items[startIndex].date = date.toISOString();
 
+    if (i < 2) {
+      items[startIndex].statusText = 'Open';
+    } else {
+      items[startIndex].statusText = 'Closed';
+    }
+
     sortedItems.push(items[startIndex]);
+
     startIndex++;
+    indexShift--;
+
+    if (indexShift < 0) indexShift = 5;
   }
 
   return sortedItems;
