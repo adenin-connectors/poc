4212a97818453d755540576857ff48211603f149
Martin Amm <ma@adenin.com>
2020-02-23 09:45:17 -0500

    revert accidential commit

--- activities/myleads.js
+++ activities/myleads.js
@@ -53,7 +53,7 @@ module.exports = async function (activity) {
 
     activity.Response.Data.thumbnail = "https://www.adenin.com/assets/images/wp-images/logo/salesforce.svg";
 
-    activity.Response.Data._card = { title: "My Leads", type: "48dc3f74-da4b-432f-bc52-0572063f848f", ztype: "at-card-status-list" };
+    // activity.Response.Data._card = { title: "My Leads", type: "48dc3f74-da4b-432f-bc52-0572063f848f", ztype: "at-card-status-list" };
 
     if (count > 0) {
       activity.Response.Data.value = count;
